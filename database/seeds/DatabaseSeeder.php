b1eb473e966123c9cbd978c7dc533ad7458d11f8
sovonthy.leng <sovonthy.leng@PNC.LAN>
2020-01-28 08:31:52 +0700

    add data 1000 record

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
-
+use Faker\Factory as Faker;
 class DatabaseSeeder extends Seeder
 {
     /**
@@ -12,5 +12,19 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
+        // DB::table('posts')->insert([
+        //     'title'=>'Code Review',
+        //     'body'=>'My favorite subject ever...'
+        // ]);
+        $faker = Faker::create();
+        for ($i = 0; $i <= 1000; $i++) {
+        DB::table('students')->insert([
+            'firstname'=>$faker->firstname(8),
+            'lastname'=>$faker->lastname(10),
+            'age'=>$faker->numberBetween($min = 15, $max = 25)
+        
+        ]);
+    }
+
     }
 }
